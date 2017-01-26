62010d17d8668ab49b2564382a3f63e0d901fb8f
DhruvinPatel <dpate78@stevens.edu>
2017-01-25 21:42:38 -0500

    second function

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -4,16 +4,40 @@
     dpate78@stevens.edu
 */
 
-// for square of a number
+// first functiion
 
 let findSquare = (x) => {
-    if( x != Number ){ 
-        return null;
+    if(typeof(x) === 'number'){ 
+        return x * x;
     }
-    return x * x;
+    return null;
 }
 
 let sumOfSquares = (num1, num2, num3) => {
-    return findSquare(num1) + findSquare(num2) + findSquare(num3);
+    return(findSquare(num1) + findSquare(num2) + findSquare(num3));
 }
+
+console.log(sumOfSquares(5,3,10));
+
+// second function
+
+function sayHelloTo(firstName,lastName,title){
+    switch(arguments.length){
+        case 1: console.log("Hello "+ firstName + "!");
+            break;
+        case 2: console.log("Hello, "+ firstName + " " + lastName + " I hope  you are having a good day!");
+            break;
+        case 3: console.log("Hello, " + title + " " + firstName + " " + lastName + "!! Have a good evening");
+            break;
+        default: console.log("/!\ Missing arguments");
+            break;
+    }
+}
+
+sayHelloTo();
+sayHelloTo("Phil");
+sayHelloTo("Phil", "Barresi");
+sayHelloTo("Phil", "Barresi", "Mr.");
+
+//third function
 
