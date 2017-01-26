faa4d449eb944a068f0885b19bc09a68161a5a4f
DhruvinPatel <dpate78@stevens.edu>
2017-01-25 20:15:16 -0500

    first func

--- lab1/lab1.js
+++ lab1/lab1.js
@@ -4,3 +4,16 @@
     dpate78@stevens.edu
 */
 
+// for square of a number
+
+let findSquare = (x) => {
+    if( x != Number ){ 
+        return null;
+    }
+    return x * x;
+}
+
+let sumOfSquares = (num1, num2, num3) => {
+    return findSquare(num1) + findSquare(num2) + findSquare(num3);
+}
+
