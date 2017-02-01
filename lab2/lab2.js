e1f6fc1d135e54d4d4206ab27ffe37747dcd3bd1
DhruvinPatel <dpate78@stevens.edu>
2017-02-01 18:21:50 -0500

    rhombus function commit

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -3,7 +3,7 @@ function tringle(n){
     let j = 0, i = 0;
     try{
         if(typeof(n) === 'number' && n > 0){
-            console.log("Drwaing tringle of height:"+n);
+            console.log(`Drwaing tringle of height:`+n);
             do{
                 let frontSpace = ' '.repeat(c);
                 if( i === n-1){
@@ -25,13 +25,13 @@ function tringle(n){
     }
 }
 
-tringle(10);
+//tringle(10);
 
 function square(n){
     let i = 0;
     try{
         if(typeof(n) === 'number' && n > 1){
-            console.log("Drwaing square having ${n} rows");
+            console.log(`Drwaing square having ${n} rows`);
             do{
                 let frontSpace = '\t';
                 if( i === n-1 || i === 0){
@@ -50,8 +50,48 @@ function square(n){
     }
 }
 
-square(1);
-square("Abc");
-square(-25);
-square(2);
-square(3);
+// square(1);
+// square("Abc");
+// square(-25);
+// square(2);
+// square(3);
+// square(10);
+
+function rhombus(n){
+    let i = 0, j = 1, c = n;
+    try{
+        if(typeof(n) === 'number' && n > 1 && n%2 === 0){
+            console.log(`Drwaing rhombus having ${n} lines`);
+            do{
+                let frontSpace = " ";
+                if( i === 0){
+                    console.log(frontSpace.repeat(n)+"/"+"-"+"\\");
+                }else if( i === n-1){
+                    console.log(frontSpace.repeat(n)+"\\"+"-"+"/");
+                }else if( i < n/2){
+                    c--;
+                    j+=2;
+                    console.log(frontSpace.repeat(c)+"/"+" ".repeat(j)+"\\");
+                }
+                else{
+                    console.log(frontSpace.repeat(c)+"\\"+" ".repeat(j)+"/");
+                    c++;
+                    j-=2;
+                }
+                i++;
+            }while(i<n);
+            console.log();
+        }else{
+            throw "Exception: Invalid Arguments";
+        }
+    }catch(e){
+        console.log(e);
+    }
+}
+
+rhombus(2);
+rhombus(3);
+rhombus(4);
+rhombus(6);
+rhombus(10);
+rhombus(42);
