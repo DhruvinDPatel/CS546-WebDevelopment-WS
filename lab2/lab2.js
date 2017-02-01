57bcadd4bda5153ddc947e0d85d36086fcc32894
DhruvinPatel <dpate78@stevens.edu>
2017-02-01 17:29:01 -0500

    square function initial commit

--- lab2/lab2.js
+++ lab2/lab2.js
@@ -1,22 +1,57 @@
-function tree(n){
+function tringle(n){
     let c = n - 1;
     let j = 0, i = 0;
-    console.log("Drwaing tree of height:"+n);
-    do{
-        let fs = ' '.repeat(c);
-        if( i === n-1){
-            console.log("/"+"-".repeat(i*2)+"\\");
-        }else{
-            console.log(fs+"/"+" ".repeat(j)+"\\");
+    try{
+        if(typeof(n) === 'number' && n > 0){
+            console.log("Drwaing tringle of height:"+n);
+            do{
+                let frontSpace = ' '.repeat(c);
+                if( i === n-1){
+                    console.log("/"+"-".repeat(i*2)+"\\");
+                }else{
+                    console.log(frontSpace+"/"+" ".repeat(j)+"\\");
+                }
+                c--;
+                j+=2;
+                i++;
+            }while(i<n);
         }
-        c--;
-        j+=2;
-        i++;
-    }while(i<n);
-    console.log("______________");
+        else{
+            throw "Exception: Invalid Arguments";
+        }
+        console.log();
+    }catch(e){
+        console.log(e);
+    }
 }
 
-tree(1);
-tree(2);
-tree(3);
-tree(25);
+tringle(10);
+
+function square(n){
+    let i = 0;
+    try{
+        if(typeof(n) === 'number' && n > 1){
+            console.log("Drwaing square having ${n} rows");
+            do{
+                let frontSpace = '\t';
+                if( i === n-1 || i === 0){
+                    console.log(frontSpace+"|"+"-".repeat(n)+"|");
+                }else{
+                    console.log(frontSpace+"|"+" ".repeat(n)+"|");
+                }
+                i++;
+            }while(i<n);
+        }else{
+            throw "Exception: Invalid Arguments";
+        }
+        console.log();
+    }catch(e){
+        console.log(e);
+    }
+}
+
+square(1);
+square("Abc");
+square(-25);
+square(2);
+square(3);
